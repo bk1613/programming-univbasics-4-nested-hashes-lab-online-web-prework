a1818cef1e6ed826680936e73b783075b6216ecc
Brian Kao <bk1613@nyu.edu>
2019-09-04 20:03:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     
     programmer_hash[:yukihiro_matsumoto][:known_for]
-    
+    programmer_hash[:yukihiro_matsumoto][:languages]
 end
 
 def changing_alan
